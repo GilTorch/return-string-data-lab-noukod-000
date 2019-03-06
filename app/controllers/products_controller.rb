46b785f19939d3cafa1003b0de62c69c47202d01
Gilbert Torchon <gilbert@noukod.com>
2019-03-06 15:46:49 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,28 @@
 class ProductsController < ApplicationController
+  def new
+    @product=Product.new
+  end
 
+  def index
+    @products=Product.all
+  end
+
+  def create
+    @product=Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      redirect_to new_product_path
+    end
+  end
+
+  def description
+  end
+
+  def inventory
+  end
+
+  def product_params
+    params.require(:product).permit(:name,:price,:inventory,:description)
+  end
 end
